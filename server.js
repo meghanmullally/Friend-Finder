405345ce764912fa0672c63cbb6cc01212508e42
Meghan Mullally <ms.mullally27@gmail.com>
2019-07-03 11:13:47 -0700

    Added start to package.json annd changed server.js process.env.port || 3000 so heroku will know what to find

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+// Dependencies 
 var express = require('express');
+
+
+// Set up the Express app 
 var app = express();
 
+// process.env.PORT (whatever it is on Heroku) or 3000
+// if process.env doesn't exist it will be 3000
+var PORT = process.env.PORT || 3000;
 
-var PORT = 3000;
 
+// basic route that sends the user first to the page 
 app.get('/', function(req, res) {
   res.send('hello world')
 });
 
+
+// starts the server to begin listening 
 app.listen(PORT, function() {
   console.log('listening on: ' + PORT)
 });
