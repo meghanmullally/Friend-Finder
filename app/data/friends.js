b56c2d1bcaf12e3451216d9db69160634603bca1
Meghan Mullally <ms.mullally27@gmail.com>
2019-07-09 22:47:04 -0700

    added friend array and objects into friend.js

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+var friends = [
+
+  {
+    name: "Lucy Jones",
+    photo: "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    scores: [
+      "3",
+      "5",
+      "1",
+      "2",
+      "4",
+      "3",
+      "1",
+      "3",
+      "5",
+      "1"
+
+    ]
+  },
+  {
+    name: "Brady Cooper",
+    photo: "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    scores: [
+      "2",
+      "1",
+      "3",
+      "4",
+      "3",
+      "3",
+      "5",
+      "1",
+      "5",
+      "1"
+
+    ]
+  },
+  {
+    name: "Yuki Kaji",
+    photo: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    scores: [
+
+      "5",
+      "1",
+      "2",
+      "4",
+      "3",
+      "1",
+      "3",
+      "5",
+      "1",
+      "3",
+
+    ]
+  }
+
+
+]
