a9fdda3b385050fba8c6d4bf93948d07fc3d6108
Meghan Mullally <ms.mullally27@gmail.com>
2019-07-09 20:21:20 -0700

    created post and get for api/friends with vars and for statement and if statment

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,75 @@
+// Dependencies
+
+var path = require('path');
+
+// import friend list 
+
+var friends = require('../data/friends');
+
+// API routes 
+
+module.exports = function (app) {
+
+  // GET route with the api/friends 
+  // used to display JSON of all possible friends 
+
+  app.get('api/friends', function (req, res) {
+    res.json(friends);
+
+  });
+
+
+  // POST route for api/friends 
+  // handle incoming survey results & add new friends 
+
+  app.post('api/friends', function (req, res) {
+
+    // the user input and response 
+
+    var userInput = req.body;
+
+    var userRep = userInput.scores;
+
+    // best friends match
+
+    var matchName = "";
+    var matchImage = "";
+    var totalDiff = 1000;
+
+    for (var i = 0; i < friends.length; i++) {
+      // difference between questions 
+      var diff = 0;
+      for (var y = 0; y < userRep.length; y++) {
+
+        // math abs returns the absolute value of a number 
+        diff += Math.abs(friends[i].scores[y] - userRep[y]);
+      }
+
+      if (diff < totalDiff) {
+
+        totalDiff = diff;
+        matchName = friends[i].name;
+        matchImage = friends[i].photo;
+
+
+      }
+
+    }
+
+    // add new friend 
+
+    friends.push(userInput);
+
+
+    // send response 
+
+    res.json({
+      status: 'OK',
+      matchName: matchName,
+      matchImage: matchImage
+    });
+
+  });
+
+
+};
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,21 @@
+// Dependencies
+
+var path = require('path');
+
+
+// API routes 
+
+module.exports = function (app) {
+
+  // GET route with the /survey 
+  // display survey page 
+  
+    app.get('/survey', function (req, res) {
+    
+
+      
+  
+    });
+  
+  
+  }
